25b75e00456524ef0b1ca6224121db394390a501
harshraoka <81157043+harshraoka@users.noreply.github.com>
2022-06-05 15:21:57 +0530

    feat : added a footer

--- client/src/Layout/Login.layout.jsx
+++ client/src/Layout/Login.layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 // component
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const LoginLayout = (props) => {
     return (
         <div>
             <Navbar className='w-full' />
             {props.children} 
+            <Footer className='w-full'></Footer>
            
         </div>
     );
