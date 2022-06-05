06628821a4d192c08dfd203b539b55a114731518
harshraoka <81157043+harshraoka@users.noreply.github.com>
2022-06-05 14:17:17 +0530

    feat:added reponsiveness to home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -33,13 +33,14 @@ const Home = () => {
     ]);
 
     return (
-        <>
+        <div className='overflow-x-hidden'>
             <div className='flex flex-col mx-16 items-center mt-5'>
                 <div className='flex w-full justify-evenly md:items-center'>
-                    <div className='container w-1/3 align-left '>
-                        <h1 className='font-bold text-4xl'>Make your loan EMIs get simplified by paying on daily basis</h1>
+                    <div className='container w-full sm:w-1/3 align-left '>
+                        <h1 className='font-bold text-4xl hidden sm:block'>Make your loan EMIs get simplified by paying on daily basis</h1>
+                        <h1 className='font-bold text-4xl block sm:hidden mt-5 mb-20'>Get your <br /> EMIs done <br /> easily</h1>
                     </div>
-                    <div className='w-1/6 h-96 items-start'>
+                    <div className='w-1/6 h-96 items-start hidden sm:block'>
                         <img
                             src={person}
                             alt="person"
@@ -57,8 +58,16 @@ const Home = () => {
                 </div>
                 <div className='flex flex-col mt-16 font-bold text-2xl w-full items-center'>
                     <h1 className='items-center'>Our Team</h1>
-                    <div className='w-full h-96 flex items-center justify-center bg-grey-50 mt-7'>
-                        <div className='flex w-full content-center gap-16 justify-evenly'>
+                    <div className='w-full h-fit p-5 flex items-center justify-center bg-grey-50 mt-7'>
+                        <div className='flex w-full content-center gap-16 justify-evenly hidden sm:flex'>
+                            {
+                                teamList.map((sources) => (
+                                    <TeamCard {...sources} />
+                                ))
+                            }
+                        </div>
+                        <div className='block content-center gap-16 sm:hidden'>
+                            
                             {
                                 teamList.map((sources) => (
                                     <TeamCard {...sources} />
@@ -69,15 +78,18 @@ const Home = () => {
                 </div>
                 <div className='flex flex-col items-center w-full mt-5'>
                     <h1 className='text-2xl font-bold'>About Us</h1>
-                    <div className='bg-grey-50 mt-10'>
-                        <div className='mx-72 mt-12 items-center text-xl text-aboutGreen-50 font-bold'>
+                    <div className='bg-grey-50 mt-10 '>
+                        <div className='flex flex-wrap w-full mt-12 text-xl text-aboutGreen-50 font-bold'>
                             <p>We are just a bunch of college students who are trying to solve the problem of small businesses who take loans from the banks.</p>
                             <p>We are bringing the idea of repayment of EMIs on daily basis so that there will be no burden of payment of EMI at the end of the month.</p>
                         </div>
                     </div>
                 </div>
+
+              
+
             </div>
-        </>
+        </div>
     );
 };
 
